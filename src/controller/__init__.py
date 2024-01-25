f25ac7bbc08caa87208164df044e6fc724239e80
Pablo González <gonzrubio.pablo@gmail.com>
2024-01-25 10:12:35 +0100

    🐛 Fix import bug

--- src/controller/__init__.py
+++ src/controller/__init__.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter
 
-from src.controller import api_v1
+from src.controller import api
 
 router = APIRouter()
-router.include_router(api_v1.router, prefix="/v1")
+router.include_router(api.router, prefix="/v1")
